fdb5a7c3f983a9f3627c99550cdad31e38cc7ea2
mioko75 <mioko75@hotmail.com>
2019-11-14 20:39:27 -0500

    organisation des fichiers

---
+++ Programmation1/Chapitre2/seminaire3-SI/js/exercise1.js
@@ -0,0 +1,19 @@
+/** created by Sophie Bergeron **/
+
+/**
+ *
+ */
+//Etape 1 trouver les variables
+
+
+//Etape 2 Lecture(demander a l'usager des données)
+
+
+
+//Etape 3 Les calcules
+
+
+
+//Etape 4 Afficher les résultats
+
+
