371f1d68415c4e39621ca6b27dd8cb290241c2b8
mioko75 <mioko75@hotmail.com>
2020-06-04 18:41:23 -0400

    build

---
+++ Programmation2/spotifyqc/build/precache-manifest.65e85433b0fe82834a603f558015fefb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c079aef1277fda503ecc938c69d8884a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "73f39653afc44641e5bb",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "84bade5601dc25ba29fe",
+    "url": "./static/js/2.5767e025.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.5767e025.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73f39653afc44641e5bb",
+    "url": "./static/js/main.c9826a46.chunk.js"
+  },
+  {
+    "revision": "e0608a2ead2bb0a4137d",
+    "url": "./static/js/runtime-main.8701e195.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
